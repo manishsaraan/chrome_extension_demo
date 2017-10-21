98fb2ae6aade38b9892a66e893ee434bb0d1d866
manishsaraan <manish021js@gmail.com>
2017-10-22 01:49:03 +0530

    context menu added

--- ProteinTracker/eventPage.js
+++ ProteinTracker/eventPage.js
@@ -1,5 +1,39 @@
-chrome.storage.onChanged.addListener(function(changes){
-	console.log("changes",changes);
+var menuItems = {
+	"id":"addProtein",
+	"title" : "Add Protein",
+	"contexts" : ["selection"]
+};
+
+//create context menu
+chrome.contextMenus.create(menuItems);
+
+//handle context menu click event
+chrome.contextMenus.onClicked.addListener(function(clickData){
+
+	//validate click
+    if(clickData.menuItemId === "addProtein" && clickData.selectionText){
+        var text = clickData.selectionText;
+    	//validate number
+    	var isnum = /^\d+$/.test(text);    	    	
+    	if(isnum){
+    		//get current total
+	         chrome.storage.sync.get('total',function(items){
+				var newTotal = 0;
+				if(items.total){
+					newTotal += parseInt(items.total);
+				}
+
+				newTotal += parseInt(text);
+				//update data
+				chrome.storage.sync.set({total: newTotal});
+			});
+    	}
+         
+    }
+});
+
+//check if storage updaged and show badge
+chrome.storage.onChanged.addListener(function(changes){	
 	//show badge
 	chrome.browserAction.setBadgeText({'text' : changes.total.newValue.toString()});
 });
