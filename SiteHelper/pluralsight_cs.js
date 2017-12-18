0fa7700b2aa5c3e15bfe4e46d805e60d093a711d
manishsaraan <manish021js@gmail.com>
2017-12-18 23:00:39 +0530

    adding sortable table

--- SiteHelper/pluralsight_cs.js
+++ SiteHelper/pluralsight_cs.js
@@ -1,1 +1,26 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+   console.log('working...');
+   if(request.action === 'courseCounts'){
+	   	var authors = $('.author a');
+			authors = $.makeArray(authors);
+			var hist = {};
+			authors.map(function (a) {
+			if (a.title in hist)
+				hist[a.title]++;
+			else
+				hist[a.title] = 1;
+			});
+			
+			for (var a in hist) {
+				var aName = '"' + a + '"';
+				var links = $('.author a[title=' + aName + ']');
+				$.each(links, function (i, v) {
+					v.innerText = a + ' - ' + hist[a];
+				});
+			}
+   }
+   if(request.action === 'makeSortable'){
+      $('.course').dataTable({"bPaginage" : false});
+   } 
+});
 chrome.runtime.sendMessage({action : 'show'});
